d5c3596568b8f733d0e67fd8301641ad55d91160
Bijan <bijanbwb@gmail.com>
2017-03-11 13:54:24 -0500

    Adding initial auth plug

---
+++ lib/platform/web/controllers/player_auth_controller.ex
@@ -0,0 +1,13 @@
+defmodule Platform.Web.PlayerAuthController do
+  import Plug.Conn
+
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  def call(conn, repo) do
+    player_id = get_session(conn, :player_id)
+    player = player_id && repo.get(Platform.Players.Player, player_id)
+    assign(conn, :current_user, player)
+  end
+end
--- lib/platform/web/router.ex
+++ lib/platform/web/router.ex
@@ -7,6 +7,7 @@ defmodule Platform.Web.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Platform.Web.PlayerAuthController, repo: Platform.Repo
   end
 
   pipeline :api do
