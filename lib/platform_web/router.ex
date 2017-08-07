4bdbaa39a5766b1da50121ded6a762ca87f5e4a6
Bijan <bijanbwb@gmail.com>
2017-08-07 08:18:39 -0400

    Update router with players resource

--- lib/platform_web/router.ex
+++ lib/platform_web/router.ex
@@ -17,6 +17,7 @@ defmodule PlatformWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/players", PlayerController
   end
 
   # Other scopes may use custom stacks.
