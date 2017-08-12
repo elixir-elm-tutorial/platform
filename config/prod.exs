1e36d547f54f344fb84c2a04cee0addbb4d5ff11
Bijan <bijanbwb@gmail.com>
2017-08-12 12:29:30 -0400

    Heroku production configuration settings

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,15 @@ use Mix.Config
 # which you typically run after static files are built.
 config :platform, PlatformWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "elixir-elm-tutorial.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+# Database configuration
+config :platform, Platform.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +68,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
