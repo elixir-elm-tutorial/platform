1e4f3f4184d86c5c8793b925e93cb5b701fbb411
Bijan <bijanbwb@gmail.com>
2017-08-20 07:50:43 -0400

    Pull in comeonin dependency

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Platform.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 4.0"}
     ]
   end
 
