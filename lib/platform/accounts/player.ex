2690f7e5c06a4f55e8d2012516ba5d6b042fb2e3
Bijan <bijanbwb@gmail.com>
2017-08-20 11:40:36 -0400

    Password hashing, test updates

--- lib/platform/accounts/player.ex
+++ lib/platform/accounts/player.ex
@@ -18,6 +18,19 @@ defmodule Platform.Accounts.Player do
   def changeset(%Player{} = player, attrs) do
     player
     |> cast(attrs, [:display_name, :password, :score, :username])
-    |> validate_required([:username])
+    |> validate_required([:password, :username])
+    |> validate_length(:username, min: 2, max: 100)
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_digest()
+  end
+
+  defp put_pass_digest(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_digest, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
