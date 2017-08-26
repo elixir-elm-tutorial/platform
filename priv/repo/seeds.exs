b1ad6b91a8dcfeb6b1d79590726b45a4ffefeeef
Bijan <bijanbwb@gmail.com>
2017-08-26 18:20:32 -0400

    Update seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,5 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Players
+
 Platform.Repo.insert!(%Platform.Accounts.Player{display_name: "José Valim", username: "josevalim", score: 1000})
 Platform.Repo.insert!(%Platform.Accounts.Player{display_name: "Evan Czaplicki", username: "evancz", score: 2000})
+Platform.Repo.insert!(%Platform.Accounts.Player{display_name: "Chris McCord", username: "chrismccord", score: 3000})
+
+# Games
+
+Platform.Repo.insert!(%Platform.Products.Game{title: "Platformer", description: "Platform game example.", thumbnail: "http://via.placeholder.com/300x200", featured: true})
