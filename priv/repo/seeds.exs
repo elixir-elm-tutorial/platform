704fbdb36f3fc20bd507f1f945bb1fc4383a97f9
Bijan <bijanbwb@gmail.com>
2017-08-27 09:22:39 -0400

    Fix seeds for sample account authentications

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,12 +10,17 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+# Aliases
+
+alias Platform.Accounts
+alias Platform.Products
+
 # Players
 
-Platform.Repo.insert!(%Platform.Accounts.Player{display_name: "José Valim", username: "josevalim", score: 1000})
-Platform.Repo.insert!(%Platform.Accounts.Player{display_name: "Evan Czaplicki", username: "evancz", score: 2000})
-Platform.Repo.insert!(%Platform.Accounts.Player{display_name: "Chris McCord", username: "chrismccord", score: 3000})
+Accounts.create_player(%{display_name: "José Valim", username: "josevalim", password: "josevalim", score: 1000})
+Accounts.create_player(%{display_name: "Evan Czaplicki", username: "evancz", password: "evancz", score: 2000})
+Accounts.create_player(%{display_name: "Chris McCord", username: "chrismccord", password: "chrismccord", score: 3000})
 
 # Games
 
-Platform.Repo.insert!(%Platform.Products.Game{title: "Platformer", description: "Platform game example.", thumbnail: "http://via.placeholder.com/300x200", featured: true})
+Products.create_game(%{title: "Platformer", description: "Platform game example.", thumbnail: "http://via.placeholder.com/300x200", featured: true})
