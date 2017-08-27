08af855ccafe1caca9ed349dd793aaddfe780a48
Bijan <bijanbwb@gmail.com>
2017-08-27 17:47:42 -0400

    Initial updates to Elm application from book content

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,8 +1,52 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+-- MAIN
 
 
 main : Html msg
 main =
-    text "Hello from Elm inside Phoenix!"
+    gamesIndex
+
+
+
+-- MODEL
+
+
+model : List String
+model =
+    [ "Platform Game"
+    , "Adventure Game"
+    ]
+
+
+firstGameMaybe : Maybe String
+firstGameMaybe =
+    List.head model
+
+
+firstGameTitle : String
+firstGameTitle =
+    Maybe.withDefault "" firstGameMaybe
+
+
+
+-- VIEW
+
+
+gamesIndex : Html msg
+gamesIndex =
+    div [ class "games-index" ] [ gamesList ]
+
+
+gamesList : Html msg
+gamesList =
+    ul [ class "games-list" ] [ gamesListItem ]
+
+
+gamesListItem : Html msg
+gamesListItem =
+    li [] [ text firstGameTitle ]
