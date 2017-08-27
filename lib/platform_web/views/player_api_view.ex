7878fa5641b258e2c5a7987118de81fdb6353185
Bijan <bijanbwb@gmail.com>
2017-08-26 23:48:01 -0400

    Add PlayerApiView

---
+++ lib/platform_web/views/player_api_view.ex
@@ -0,0 +1,16 @@
+defmodule PlatformWeb.PlayerApiView do
+  use PlatformWeb, :view
+  alias PlatformWeb.PlayerApiView
+
+  def render("index.json", %{players: players}) do
+    %{data: render_many(players, PlayerApiView, "player.json")}
+  end
+
+  def render("show.json", %{player: player}) do
+    %{data: render_one(player, PlayerApiView, "player.json")}
+  end
+
+  def render("player.json", %{player: player}) do
+    %{id: player.id}
+  end
+end
