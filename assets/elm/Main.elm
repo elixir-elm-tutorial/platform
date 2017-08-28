67f8c103e0d4333b09e5d51a27880d13cd3dac00
Bijan <bijanbwb@gmail.com>
2017-08-28 09:29:22 -0400

    Updates from Elm Architecture chapter

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -2,44 +2,111 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 -- MAIN
 
 
-main : Html msg
+main : Program Never Model Msg
 main =
-    div []
-        [ h1 [] [ text "Games" ]
-        , gamesIndex model
-        ]
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
 
-model : List String
-model =
-    [ "Platform Game"
-    , "Adventure Game"
-    ]
+type alias Model =
+    { gamesList : List Game
+    , displayGamesList : Bool
+    }
+
+
+type alias Game =
+    { gameTitle : String
+    , gameDescription : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { gamesList =
+        [ { gameTitle = "Platform Game", gameDescription = "Platform game example." }
+        , { gameTitle = "Adventure Game", gameDescription = "Adventure game example." }
+        ]
+    , displayGamesList = False
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = DisplayGamesList
+    | HideGamesList
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        DisplayGamesList ->
+            ( { model | displayGamesList = True }, Cmd.none )
+
+        HideGamesList ->
+            ( { model | displayGamesList = False }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
 -- VIEW
 
 
-gamesIndex : List String -> Html msg
-gamesIndex gameTitles =
-    div [ class "games-index" ] [ gamesList gameTitles ]
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [ class "games-section" ] [ text "Games" ]
+        , button [ class "btn btn-success", onClick DisplayGamesList ] [ text "Display Games List" ]
+        , button [ class "btn btn-danger", onClick HideGamesList ] [ text "Hide Games List" ]
+        , if model.displayGamesList then
+            gamesIndex model
+          else
+            div [] []
+        ]
 
 
-gamesList : List String -> Html msg
-gamesList gameTitles =
-    ul [ class "games-list" ] (List.map gamesListItem gameTitles)
+gamesIndex : Model -> Html msg
+gamesIndex model =
+    div [ class "games-index" ] [ gamesList model.gamesList ]
 
 
-gamesListItem : String -> Html msg
-gamesListItem gameTitle =
-    li [] [ text gameTitle ]
+gamesList : List Game -> Html msg
+gamesList games =
+    ul [ class "games-list" ] (List.map gamesListItem games)
+
+
+gamesListItem : Game -> Html msg
+gamesListItem game =
+    li [ class "game-item" ]
+        [ strong [] [ text game.gameTitle ]
+        , p [] [ text game.gameDescription ]
+        ]
