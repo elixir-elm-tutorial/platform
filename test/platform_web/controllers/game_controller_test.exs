bac54bfd42113324a14d680ea20d4414be576d42
Bijan <bijanbwb@gmail.com>
2017-09-13 23:28:46 -0400

    Passing tests

--- test/platform_web/controllers/game_controller_test.exs
+++ test/platform_web/controllers/game_controller_test.exs
@@ -35,7 +35,8 @@ defmodule PlatformWeb.GameControllerTest do
         "description" => "some description",
         "featured" => true,
         "thumbnail" => "some thumbnail",
-        "title" => "some title"}
+        "title" => "some title",
+        "slug" => "some-slug"}
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
@@ -57,7 +58,8 @@ defmodule PlatformWeb.GameControllerTest do
         "description" => "some updated description",
         "featured" => false,
         "thumbnail" => "some updated thumbnail",
-        "title" => "some updated title"}
+        "title" => "some updated title",
+        "slug" => "some-slug"}
     end
 
     test "renders errors when data is invalid", %{conn: conn, game: game} do
