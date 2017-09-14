2ce8c50000af15f04924fbc2690008818befbc80
Bijan <bijanbwb@gmail.com>
2017-09-14 00:52:13 -0400

    Configure brunch with executablePath

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -12,6 +12,7 @@ exports.config = {
   plugins: {
     babel: { ignore: [/vendor/] },
     elmBrunch: {
+      executablePath: "./node_modules/elm/binwrappers",
       mainModules: ["elm/Main.elm", "elm/Platformer.elm"],
       makeParameters: ["--debug"],
       outputFile: "elm.js",
