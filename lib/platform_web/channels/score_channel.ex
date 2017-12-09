986ce7e48d98152aa71806bbdf26c8bdf816535f
Bijan <bijanbwb@gmail.com>
2017-12-09 13:54:38 -0500

    New channel message for saving scores

--- lib/platform_web/channels/score_channel.ex
+++ lib/platform_web/channels/score_channel.ex
@@ -18,10 +18,16 @@ defmodule PlatformWeb.ScoreChannel do
     {:reply, {:ok, payload}, socket}
   end
 
+  # Save score to the database as Gameplay record.
+  def handle_in("save_score", %{"player_score" => player_score} = payload, socket) do
+    Products.create_gameplay(%{player_id: socket.assigns.player_id, game_id: socket.assigns.game_id, player_score: player_score})
+    broadcast(socket, "sync_score", payload)
+    {:noreply, socket}
+  end
+
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (score:lobby).
-  def handle_in("shout", %{"player_score" => player_score} = payload, socket) do
-    Products.create_gameplay(%{player_id: socket.assigns.player_id, game_id: socket.assigns.game_id, player_score: player_score})
+  def handle_in("shout", payload, socket) do
     broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
