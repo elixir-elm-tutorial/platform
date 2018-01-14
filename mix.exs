36fda3277271c659aa61430ec0e958f4a0e55538
Bijan <bijanbwb@gmail.com>
2018-01-14 10:24:10 -0500

    Finish dependency updates
    Managed to get everything updated except cowboy, which looks like it
    would require an update from Phoenix before Cowboy 2 works.

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Platform.Mixfile do
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:comeonin, "~> 4.0"},
-      {:bcrypt_elixir, "~> 0.12"}
+      {:bcrypt_elixir, "~> 1.0"}
     ]
   end
 
