3af3a7f90af11cd3563ea48dfd44ca10a7e2234e
Bijan <bijanbwb@gmail.com>
2018-01-14 10:11:59 -0500

    Resolve compilation errors, update Elixir version

--- lib/platform_web/channels/score_channel.ex
+++ lib/platform_web/channels/score_channel.ex
@@ -1,16 +1,15 @@
 defmodule PlatformWeb.ScoreChannel do
   use PlatformWeb, :channel
 
-  alias Platform.Accounts
   alias Platform.Products
 
-  def join("score:" <> game_slug, payload, socket) do
+  def join("score:" <> game_slug, _payload, socket) do
     game = Products.get_game_by_slug!(game_slug)
     socket = assign(socket, :game_id, game.id)
     {:ok, socket}
   end
 
-  def handle_in("save_score", %{"player_score" => player_score} = payload, socket) do
+  def handle_in("save_score", %{"player_score" => player_score}, socket) do
     payload = %{
       player_score: player_score,
       game_id: socket.assigns.game_id,
