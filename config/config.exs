8785375488cb628adaecbc0a3aee212b7e52b265
Bijan <bijanbwb@gmail.com>
2018-03-09 17:07:50 -0500

    Run mix format on config files

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :platform,
-  ecto_repos: [Platform.Repo]
+config :platform, ecto_repos: [Platform.Repo]
 
 # Configures the endpoint
 config :platform, PlatformWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "5WY2LoKih9wBLcSs1KJdSumLNMFDzgDYvhqwdZjFvSVgDDVIYjx+dHmikCho8/lm",
   render_errors: [view: PlatformWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Platform.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Platform.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +22,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
