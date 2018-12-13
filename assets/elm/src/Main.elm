356f84ec3a27c92c5378099cb72aede01e1089cf
Bijan <bijanbwb@gmail.com>
2018-12-12 20:50:58 -0500

    Changes from updated Elm API chapter

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
 
 
 
@@ -25,29 +27,98 @@ main =
 
 type alias Model =
     { gamesList : List Game
+    , playersList : List Player
     }
 
 
 type alias Game =
-    { title : String
-    , description : String
+    { description : String
+    , featured : Bool
+    , id : Int
+    , thumbnail : String
+    , title : String
+    }
+
+
+type alias Player =
+    { displayName : Maybe String
+    , id : Int
+    , score : Int
+    , username : String
     }
 
 
 initialModel : Model
 initialModel =
     { gamesList = []
+    , playersList = []
     }
 
 
 initialCommand : Cmd Msg
 initialCommand =
-    Cmd.none
+    Cmd.batch
+        [ fetchGamesList
+        , fetchPlayersList
+        ]
 
 
-init : ( Model, Cmd Msg )
-init =
+init : () -> ( Model, Cmd Msg )
+init _ =
     ( initialModel, initialCommand )
+
+
+
+-- API
+
+
+fetchGamesList : Cmd Msg
+fetchGamesList =
+    Http.get
+        { url = "/api/games"
+        , expect = Http.expectJson FetchGamesList decodeGamesList
+        }
+
+
+fetchPlayersList : Cmd Msg
+fetchPlayersList =
+    Http.get
+        { url = "/api/players"
+        , expect = Http.expectJson FetchPlayersList decodePlayersList
+        }
+
+
+decodeGamesList : Decode.Decoder (List Game)
+decodeGamesList =
+    decodeGame
+        |> Decode.list
+        |> Decode.at [ "data" ]
+
+
+decodeGame : Decode.Decoder Game
+decodeGame =
+    Decode.map5 Game
+        (Decode.field "description" Decode.string)
+        (Decode.field "featured" Decode.bool)
+        (Decode.field "id" Decode.int)
+        (Decode.field "thumbnail" Decode.string)
+        (Decode.field "title" Decode.string)
+
+
+decodePlayersList : Decode.Decoder (List Player)
+decodePlayersList =
+    decodePlayer
+        |> Decode.list
+        |> Decode.at [ "data" ]
+
+
+decodePlayer : Decode.Decoder Player
+decodePlayer =
+    Decode.map4 Player
+        (Decode.maybe (Decode.field "display_name" Decode.string))
+        (Decode.field "id" Decode.int)
+        (Decode.field "score" Decode.int)
+        (Decode.field "username" Decode.string)
 
 
 
@@ -55,14 +126,30 @@ init =
 
 
 type Msg
-    = FetchGamesList
+    = FetchGamesList (Result Http.Error (List Game))
+    | FetchPlayersList (Result Http.Error (List Player))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        FetchGamesList ->
-            ( { model | gamesList = [] }, Cmd.none )
+        FetchGamesList result ->
+            case result of
+                Ok games ->
+                    ( { model | gamesList = games }, Cmd.none )
+
+                Err _ ->
+                    Debug.log "Error fetching games from API."
+                        ( model, Cmd.none )
+
+        FetchPlayersList result ->
+            case result of
+                Ok players ->
+                    ( { model | playersList = players }, Cmd.none )
+
+                Err _ ->
+                    Debug.log "Error fetching players from API."
+                        ( model, Cmd.none )
 
 
 
@@ -80,11 +167,79 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    div []
+        [ gamesIndex model
+        , playersIndex model
+        ]
+
+
+
+-- GAMES
+
+
+gamesIndex : Model -> Html msg
+gamesIndex model =
     if List.isEmpty model.gamesList then
         div [] []
 
     else
-        div []
-            [ h1 [ class "games-section" ] [ text "Games" ]
-            , gamesIndex model
+        div [ class "games-index" ]
+            [ h2 [] [ text "Games" ]
+            , gamesList model.gamesList
             ]
+
+
+gamesList : List Game -> Html msg
+gamesList games =
+    ul [ class "games-list" ] (List.map gamesListItem games)
+
+
+gamesListItem : Game -> Html msg
+gamesListItem game =
+    li [ class "game-item" ]
+        [ strong [] [ text game.title ]
+        , p [] [ text game.description ]
+        ]
+
+
+
+-- PLAYERS
+
+
+playersIndex : Model -> Html msg
+playersIndex model =
+    if List.isEmpty model.playersList then
+        div [] []
+
+    else
+        div [ class "players-index" ]
+            [ h2 [] [ text "Players" ]
+            , model.playersList
+                |> playersSortedByScore
+                |> playersList
+            ]
+
+
+playersSortedByScore : List Player -> List Player
+playersSortedByScore players =
+    players
+        |> List.sortBy .score
+        |> List.reverse
+
+
+playersList : List Player -> Html msg
+playersList players =
+    ul [ class "players-list" ] (List.map playersListItem players)
+
+
+playersListItem : Player -> Html msg
+playersListItem player =
+    li [ class "player-item" ]
+        [ case player.displayName of
+            Just displayName ->
+                strong [] [ text displayName ]
+
+            Nothing ->
+                strong [] [ text player.username ]
+        , p [] [ text (String.fromInt player.score) ]
+        ]
