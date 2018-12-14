8d5cc122f60991311a1f2da13411da0d6ec54628
Bijan <bijanbwb@gmail.com>
2018-12-14 08:11:49 -0500

    Import working content from Our First Game chapter

--- assets/elm/src/Games/Platformer.elm
+++ assets/elm/src/Games/Platformer.elm
@@ -1,8 +1,149 @@
 module Games.Platformer exposing (main)
 
-import Html exposing (..)
+import Browser
+import Html exposing (Html, div)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
-main : Html msg
+
+-- MAIN
+
+
 main =
-    text "Platformer Game"
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { characterPositionX : Int
+    , characterPositionY : Int
+    , itemPositionX : Int
+    , itemPositionY : Int
+    }
+
+
+initialModel : Model
+initialModel =
+    { characterPositionX = 50
+    , characterPositionY = 300
+    , itemPositionX = 500
+    , itemPositionY = 300
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ viewGame model ]
+
+
+viewGame : Model -> Svg Msg
+viewGame model =
+    svg [ version "1.1", width "600", height "400" ]
+        [ viewGameWindow
+        , viewGameSky
+        , viewGameGround
+        , viewCharacter model
+        , viewItem model
+        ]
+
+
+viewGameWindow : Svg Msg
+viewGameWindow =
+    rect
+        [ width "600"
+        , height "400"
+        , fill "none"
+        , stroke "black"
+        ]
+        []
+
+
+viewGameSky : Svg Msg
+viewGameSky =
+    rect
+        [ x "0"
+        , y "0"
+        , width "600"
+        , height "300"
+        , fill "#4b7cfb"
+        ]
+        []
+
+
+viewGameGround : Svg Msg
+viewGameGround =
+    rect
+        [ x "0"
+        , y "300"
+        , width "600"
+        , height "100"
+        , fill "green"
+        ]
+        []
+
+
+viewCharacter : Model -> Svg Msg
+viewCharacter model =
+    image
+        [ xlinkHref "/images/character.gif"
+        , x (String.fromInt model.characterPositionX)
+        , y (String.fromInt model.characterPositionY)
+        , width "50"
+        , height "50"
+        ]
+        []
+
+
+viewItem : Model -> Svg Msg
+viewItem model =
+    image
+        [ xlinkHref "/images/coin.svg"
+        , x (String.fromInt model.itemPositionX)
+        , y (String.fromInt model.itemPositionY)
+        , width "20"
+        , height "20"
+        ]
+        []
